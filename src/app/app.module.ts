88ab418d17438f0f667f42ff3c0efcfcf6bfd97e
huymach91 <huy.mach.webdev@gmail.com>
2021-10-04 11:09:42 +0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,47 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {
-  INgxSimpleTableChecked,
-  INgxSimpleTableConfig,
-} from './ngx-simple-table/ngx-simple-table.interface';
 import { AppService } from './app.service';
+import { NgxSimpleTableModule } from './ngx-simple-table/ngx-simple-table.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgxSimpleTableModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   providers: [AppService],
 })
-export class AppModule {
-  public simpleTableConfig: INgxSimpleTableConfig = {
-    tableClass: 'my-simple-table',
-    virtualScroll: false,
-    columns: [
-      {
-        prop: 'id',
-        title: 'ID',
-      },
-      {
-        prop: 'name',
-        title: 'Name',
-      },
-      {
-        prop: 'age',
-        title: 'Age',
-      },
-    ],
-  };
-  public tableChecked: INgxSimpleTableChecked = {
-    isCheckAll: false,
-    itemsChecked: [],
-    itemsRemoved: [],
-    indeterminate: false,
-  };
-  public data = [];
-  public pagination = {
-    pageSize: 10,
-    currentPage: 1,
-    totalRecord: 0,
-  };
-}
+export class AppModule {}
