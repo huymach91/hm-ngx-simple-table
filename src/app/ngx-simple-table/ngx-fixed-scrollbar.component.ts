c2586cf6d6258bef501cfb7da085c66b69eddb99
huymach91 <huy.mach.webdev@gmail.com>
2021-10-06 16:10:47 +0700

    2 files modified

--- src/app/ngx-simple-table/ngx-fixed-scrollbar.component.ts
+++ src/app/ngx-simple-table/ngx-fixed-scrollbar.component.ts
@@ -2,6 +2,7 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  HostListener,
   Input,
   OnInit,
   ViewChild,
@@ -13,11 +14,15 @@ import {
   styleUrls: ['./ngx-fixed-scrollbar.component.scss'],
 })
 export class NgxFixedScrollbarComponent implements OnInit, AfterViewInit {
-  @ViewChild('content') content: ElementRef;
+  @ViewChild('contentRef') contentRef: ElementRef;
   @ViewChild('wrapperRef') wrapperRef: ElementRef;
   @ViewChild('innerRef') innerRef: ElementRef;
 
-  @Input('fixedScrollbar') fixedScrollbar: boolean;
+  @Input('fixed') fixed: boolean;
+
+  private content: HTMLDivElement;
+  private scrollbarWrapper: HTMLDivElement;
+  private scrollbarInner: HTMLDivElement;
 
   constructor(private element: ElementRef) {}
 
@@ -27,22 +32,47 @@ export class NgxFixedScrollbarComponent implements OnInit, AfterViewInit {
     this.init();
   }
 
+  @HostListener('document:scroll', ['$event'])
+  public onWindowScroll(event: any) {
+    // scrollbar wrapper
+    const scrollbarOffsetTop = this.scrollbarWrapper.offsetTop;
+    const scrollbarRect = this.scrollbarWrapper.getBoundingClientRect();
+    // view port
+    const viewPortBottomEgde = scrollbarRect.y + window.innerHeight;
+    // case 1: view port's bottom edge was greater or equal the offset top of the scrollbar. In other word, bottom edge touches the scrollbar position
+    if (viewPortBottomEgde >= scrollbarOffsetTop) {
+      return;
+    }
+    // case 2: user scroll up the window
+  }
+
   public init() {
     // content
-    const contentWrapper = this.content.nativeElement as HTMLDivElement;
-    const contentWrapperWidth = contentWrapper.offsetWidth;
-    const contentWidth = contentWrapper.querySelector('table').offsetWidth;
+    this.content = this.contentRef.nativeElement as HTMLDivElement;
+    const contentWrapperWidth = this.content.offsetWidth;
+    const contentWidth = this.content.querySelector('table').offsetWidth;
 
     //wrapper scrollbar
-    const scrollbarWrapper = this.wrapperRef.nativeElement as HTMLDivElement;
-    scrollbarWrapper.style.setProperty('width', contentWrapperWidth + 'px');
-    scrollbarWrapper.onscroll = (event: any) => {
-      const scrollLeft = scrollbarWrapper.scrollLeft;
-      contentWrapper.scrollLeft = scrollLeft;
+    this.scrollbarWrapper = this.wrapperRef.nativeElement as HTMLDivElement;
+    this.scrollbarWrapper.style.setProperty(
+      'width',
+      contentWrapperWidth + 'px'
+    );
+    this.scrollbarWrapper.onscroll = (event: any) => {
+      const scrollLeft = this.scrollbarWrapper.scrollLeft;
+      this.content.scrollLeft = scrollLeft;
     };
 
     // inner scrollbar
-    const inner = this.innerRef.nativeElement as HTMLDivElement;
-    inner.style.setProperty('width', contentWidth + 'px');
+    this.scrollbarInner = this.innerRef.nativeElement as HTMLDivElement;
+    this.scrollbarInner.style.setProperty('width', contentWidth + 'px');
   }
+
+  private startFixed() {
+    // content
+    const contentOffsetLeft = this.content.offsetLeft;
+    const scrollbarWrapper = this.wrapperRef.nativeElement as HTMLDivElement;
+  }
+
+  private stopFixed() {}
 }
