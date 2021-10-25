bc712691657e5488cd3a26aa3b16994802070c8f
huymach91 <huy.mach.webdev@gmail.com>
2021-10-25 19:08:09 +0700

    1 file modified

--- src/app/demo-fixed-column-table.component.ts
+++ src/app/demo-fixed-column-table.component.ts
@@ -19,10 +19,12 @@ export class DemoFixedColumnTableComponent implements OnInit {
       {
         prop: 'id',
         title: 'ID',
+        fixed: true,
       },
       {
         prop: 'name',
         title: 'Name',
+        fixed: true,
       },
       {
         prop: 'age',
