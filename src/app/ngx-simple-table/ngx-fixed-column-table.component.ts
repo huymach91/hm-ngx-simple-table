cec0d4e73746d89016290037ac1804c222123977
huymach91 <huy.mach.webdev@gmail.com>
2021-10-26 10:53:36 +0700

    1 file modified

--- src/app/ngx-simple-table/ngx-fixed-column-table.component.ts
+++ src/app/ngx-simple-table/ngx-fixed-column-table.component.ts
@@ -298,6 +298,8 @@ export class NgxFixedColumnTableComponent implements OnInit, AfterViewInit {
     const fixedHeaderInner = this.fixedHeaderInnerRef
       .nativeElement as HTMLDivElement;
     // const diff = 1;
+    const firstRightCell = tableElement.querySelector('.first-right-cell');
+    console.log('firstRightCell', firstRightCell);
 
     fixedHeaderInner.style.setProperty(
       'width',
@@ -305,6 +307,7 @@ export class NgxFixedColumnTableComponent implements OnInit, AfterViewInit {
     );
     // 2px of this padding due to the table has 2px on the left when moving fixed columns to left side, this create an extra 2px scrollLeft;
     // fixedHeaderInner.style.setProperty('padding-right', '2px');
+
     fixedHeaderWrapper.style.setProperty('width', contentWidth + 'px');
     fixedHeaderWrapper.style.setProperty(
       'left',
